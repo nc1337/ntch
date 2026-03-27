a17fdc850d6d69d9cc9d1ad2f258425c287cd403
Nitin Chaudhary <nitinmax1000@gmail.com>
2026-03-28 04:45:54 +0530

    added my basic detail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <div> This is gonna be my portfolio webstie</div>
+      <h1>Nitin Chaudhary</h1>
+      <h2>Front End Software Developer having 7 years of experience based in India.</h2>
     </div>
   );
 }
